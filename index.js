b0e55ea63fb0abbbb83b5593f1f6799c8020a375
Iker Karkokli <iturriker@gmail.com>
2025-02-07 13:55:41 +0100

    directories reorganized and minnor css cleaning

--- index.js
+++ index.js
@@ -1,42 +1,34 @@
 // FOOTER
 function getYear() {
-    var currentYear = new Date().getFullYear();
-    document.getElementById('year').textContent = currentYear;
+    document.getElementById('year').textContent = new Date().getFullYear();
 }
 
 // HEADER & BURGER BUTTON
-var burgerButton = document.getElementById("burger-button");
-var header = document.getElementById("header");
+const burgerButton = document.getElementById("burger-button");
+const header = document.getElementById("header");
 
 function handleHeaderScroll() {
-    if (document.documentElement.scrollTop > 0)
-        {
+    if (document.documentElement.scrollTop > 0) {
             header.classList.add("hidden");
             burgerButton.classList.remove("hidden");
-        }
-    else
-        {
+    } else {
             header.classList.remove("hidden");
             burgerButton.classList.add("hidden");
-        }
-    
+    }
     burgerButton.classList.remove("active");
     header.classList.remove("opaque");
 }
 
 function handleBurgerButtonClick() {
-    if (burgerButton.classList.contains("active"))
-        {
+    if (burgerButton.classList.contains("active")) {
             burgerButton.classList.remove("active");
             header.classList.add("hidden");
             header.classList.remove("opaque");
-        }
-    else
-        {
+    } else {
             burgerButton.classList.add("active");
             header.classList.remove("hidden");
             header.classList.add("opaque");
-        }
+    }
 }
 
 // EVENTS
